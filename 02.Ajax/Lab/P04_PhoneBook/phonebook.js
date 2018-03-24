c11d20850a6ac047bb113fd49d11dd75d7f45a75
tsekotsolov <tsekotsolov@gmail.com>
2018-03-24 13:57:22 +0200

    phonebook solved

--- 02.Ajax/Lab/P04_PhoneBook/phonebook.js
+++ 02.Ajax/Lab/P04_PhoneBook/phonebook.js
@@ -1,6 +1,5 @@
 const url = 'https://phonebook-e7426.firebaseio.com/phonebook';
 
-
 $('#btnLoad').click(loadData);
 $('#btnCreate').click(createData);
 let container = $('#phonebook');
@@ -8,25 +7,13 @@ let container = $('#phonebook');
 function loadData() {
 
   $.ajax({
-    method: "GET",
-    url: url + ".json",
-    success: function (response) {
-
-      container.empty();
-      for (const record in response) {
-
-        container.append($('<li>').text(`${response[record].name}: ${response[record].phone} `)
-          .append($('<a href ="#">').text('[Delete]')).click((e) => {
-            $.ajax({
-              method: "DELETE",
-              url: url + `/${record}.json`,
-              success: function (response) {
-                e.target.parentElement.remove();
-              },
-              error: errorHandler
-            })
-          }));
-      };
+      method: "GET",
+      url: url + ".json",
+      success: function (response) {
+        container.empty();
+        for (const record in response) {
+          generateListItem(response[record].name,response[record].phone,record)
+        }
     },
     error: errorHandler
   })
@@ -50,8 +37,8 @@ function createData() {
       method: "POST",
       url: url + '/.json',
       data: JSON.stringify(record),
-      success: function () {
-        loadData();
+      success: function (response) {
+        generateListItem(record.name,record.phone,response.name);
       },
       error: errorHandler
     })
@@ -61,6 +48,22 @@ function createData() {
 
 }
 
+function generateListItem(name,phone,ID) {
+
+  container.append($('<li>').text(`${name}: ${phone} `)
+    .append($('<a href ="#">').text('[Delete]')).click((e) => {
+      $.ajax({
+        method: "DELETE",
+        url: url + `/${ID}.json`,
+        success: function () {
+          e.target.parentElement.remove();
+        },
+        error: errorHandler
+      })
+    }));
+}
+
 function errorHandler(error) {
   container.append($('<li>').text("Error"))
 }
+
