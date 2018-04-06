5004e3378bf7987996baf3432f6984a0a7c9a9a7
tsekotsolov <tsekotsolov@gmail.com>
2018-04-06 21:50:27 +0300

    up

--- 05.Templating/Lab/app.js
+++ 05.Templating/Lab/app.js
@@ -4,20 +4,19 @@ $(() => {
 
         let sourceContacts = await $.get('./contacts-template.hbs');
         let sourceDetails = await $.get('./details-template.hbs');
-        let templateDetails = Handlebars.compile(sourceDetails);
-        
         let data = await $.get('./data.json');
+        
         let dataObj = {
             contacts: data
         }
 
         let templateContacts = Handlebars.compile(sourceContacts);
+        let templateDetails = Handlebars.compile(sourceDetails);
         let instanceOfTemplateContacts = templateContacts(dataObj);
         $('#list').append($(instanceOfTemplateContacts).click(function () {
+            
             let clickedPerson = data[($(this).attr('data-id'))];
-
             let instanceOfTemplateDetails = templateDetails(clickedPerson);
-     
             $('.content').empty();
             $('.content').append(instanceOfTemplateDetails)
 
